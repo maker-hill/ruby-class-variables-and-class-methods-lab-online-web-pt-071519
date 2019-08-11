bc0c0446fd2b888ccee9fab8d105ad5964cf6acd
Jeremy Hill <j.c.hillmail@gmail.com>
2019-08-11 12:19:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song 
+  
+attr_accessor :name , :artist , :genre
+@@count = 0 
+@@genres = []
+@@artists = []
+def initialize(name, artist, genre)
+  
+  @@count += 1
+  @@genres << genre
+  @@artists << artist
+  
+end
+
+def self.count
+  @@count
+
+def self.genres 
+  @@genres
+end
+  
+def artists
+  @@artists 
+  
+end 
+
+def genre_count
+  @@g_count = Hash.new 
+  @@genres.each do |g|
+    @@g_count[g] = 
+end  
+
+end
+
